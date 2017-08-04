e9cca7feb52993cf3b7f8baaed3eda4169760ac8
DomDuxbury <domduxbury@hotmail.com>
2017-08-04 16:47:51 +0100

    Created yes/no question util

--- source/main.py
+++ source/main.py
@@ -4,6 +4,8 @@ import utils
 def main():
     age = utils.ask_question("How old are you")
     print age
+    yes = utils.ask_yes_no_question("Yes? (y/n)")
+    print yes
 
 
 if __name__ == "__main__":
--- source/utils.py
+++ source/utils.py
@@ -4,6 +4,20 @@ from sys import stdin
 def ask_question(question):
 
     print question
-
     answer = stdin.readline()
     return answer
+
+
+def ask_yes_no_question(question):
+    """
+    Takes a string as a question to print
+    Returns True or False as an answer
+    """
+    valid_response = False
+    while not valid_response:
+        answer = ask_question(question)
+        if answer == "y\n":
+            valid_response = True
+        if answer == "n\n":
+            valid_response = True
+    return answer == "y\n"
