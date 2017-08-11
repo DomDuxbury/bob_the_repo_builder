bca16898d9999e7ef80977a5a91738b015c3cbab
DomDuxbury <domduxbury@hotmail.com>
2017-08-11 16:04:40 +0100

    Moved templates into render

--- source/main.py
+++ source/main.py
@@ -8,6 +8,20 @@ def main():
     os.mkdir(set_up["name"])
 
 
+def create_repo(set_up):
+    # Create and run Bash
+        # Create project folder
+        # Create Venv
+        # Install flake8
+        # Install pytest
+        # Create requirements.txt
+        # Initialise git
+        # Install pytest watch
+        # Install precommit hooks
+    # Create README
+    # Create gitignore
+
+
 def ask_all_questions():
 
     project_name = utils.ask_question("name:")
