4faabebf59c97ab23b54f86d4dec67d9f1a461d9
DomDuxbury <domduxbury@hotmail.com>
2017-08-11 17:05:55 +0100

    Scripts runs

--- source/render/interface.py
+++ source/render/interface.py
@@ -1,4 +1,6 @@
 import utils
+import os
+import subprocess
 
 # {'git': False, 'venv': False, 'name': 'joezcool',
 # 'flake': False, 'pytest': False}
@@ -16,9 +18,22 @@ import utils
 # Create gitignore
 
 
-def create_install_script(set_up):
-    install_loc = './render/templates/install-template.txt'
+def magic(file_path):
+    mode = os.stat(file_path).st_mode
+    mode |= (mode & 0o444) >> 2
+    os.chmod(file_path, mode)
+
+
+def run_install_script(set_up):
     install_output = set_up['name'] + '/' + 'install.sh'
+    create_install_script(set_up, install_output)
+    magic(install_output)
+    os.chdir(set_up['name'])
+    subprocess.call('./install.sh', shell=True)
+
+
+def create_install_script(set_up, install_output):
+    install_loc = './render/templates/install-template.txt'
     utils.render(install_loc, install_output, set_up)
 
 
@@ -29,4 +44,4 @@ def create_repo(set_up):
         readme_loc = template_folder + 'README-template.txt'
         utils.render(readme_loc, readme_output, set_up)
 
-    create_install_script(set_up)
+    run_install_script(set_up)
